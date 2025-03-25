569e4bcfbb5f22bde0e1fa606f467d6935857a86
angeloalmanza <angeloalmanza26@gmail.com>
2025-03-25 13:11:58 +0100

    feat: modello con migrazione e seeder, controller con le crud

---
+++ database/seeders/ProjectsTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Project;
+
+use Faker\Generator as Faker;
+
+class ProjectsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        for($i = 0; $i < 10; $i++){
+            $newProject = new Project();
+
+            $startDate = $faker->dateTimeBetween('-1 year', 'now');
+            $endDate = $faker->dateTimeBetween($startDate, '+6 months');
+
+            $newProject->name = $faker->sentence();
+            $newProject->client = $faker->name();
+            $newProject->start_date = $startDate->format('Y-m-d');
+            $newProject->end_date = $endDate->format('Y-m-d');
+            $newProject->description = $faker->paragraphs(3, true);
+
+            $newProject->save();
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\ProjectController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -25,5 +26,8 @@ Route::middleware(['auth', 'verified'])
         Route::get("/", [DashboardController::class, 'index'])
             ->name('index');
     });
+
+Route::resource("projects", ProjectController::class)
+    ->middleware(['auth', 'verified']);
 
 require __DIR__.'/auth.php';
